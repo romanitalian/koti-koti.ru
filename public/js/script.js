ffc172b6608cfe1672f35a589c61ee01e6216a7b
romanitalian <romanitalian.net@gmail.com>
2020-04-04 09:39:33 +0300

    rm generated content, leave only src

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-window.onload=function(){
-	var video = document.getElementsByTagName('video')[0];
-	if (video) {
-		video.addEventListener('click',function(){
-		    video.play();
-		},false);
-	}
-}
